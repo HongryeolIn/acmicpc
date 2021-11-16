a543b0fc1f0381a3d02db48259292c1c1ffd3665
Hongryeol In <phbzti924@naver.com>
2021-11-16 19:10:11 +0900

    Create 5338.cpp

---
+++ 5338.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << "       _.-;;-._\n"
+			"'-..-'|   ||   |\n"
+			"'-..-'|_.-;;-._|\n"
+			"'-..-'|   ||   |\n"
+			"'-..-'|_.-''-._|";
+}
