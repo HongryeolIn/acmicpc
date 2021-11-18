c9965b67b3e3d0234a376340466429b10347758f
Hongryeol In <phbzti924@naver.com>
2021-11-18 19:41:55 +0900

    Create 5086.cpp

---
+++ 5086.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	int a, b;
+	while(cin >> a >> b){
+		if(a==0&&b==0) return 0;
+		if(a%b == 0) cout << "multiple" << endl;
+		else if(b%a == 0) cout << "factor" << endl;
+		else cout << "neither" << endl;
+	}
+	return 0;
+}
