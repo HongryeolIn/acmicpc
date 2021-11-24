2e0991e209be56b5c5de8a2a60b86cf80afc87cd
Hongryeol In <phbzti924@naver.com>
2021-11-24 20:36:50 +0900

    Create 10870.cpp

---
+++ 10870.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int fibo(int n){
+	if(n == 0) return 0;
+	if(n == 1) return 1;
+	return fibo(n-2) + fibo(n-1);
+}
+
+int main(void)
+{
+	int n;
+	cin >> n;
+	cout << fibo(n) << endl;
+}
