f8c30f963539c7218a83e029a9c6d5e44b418288
Hongryeol In <phbzti924@naver.com>
2021-12-06 02:36:18 +0900

    Create 2587.cpp

---
+++ 2587.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(void)
+{
+	int arr[5];
+	int middle, avg=0;
+	for(int i = 0; i<5 ;i++) {
+		cin >> arr[i];
+		avg += arr[i];
+	}
+	sort(arr,arr+5);
+	cout << avg/5 << endl << arr[2] << endl;
+}
